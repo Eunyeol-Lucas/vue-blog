3ef8e7b9d63b4956945b33dddfbebfea9a32ff82
Eunyeol <ney9083@gmail.com>
2022-05-05 22:33:57 +0900

    Feat: 초기 세팅 및 Navigation 구현

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, serverTimestamp } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCo9ba1efcljVC4ZMsqpgdftF9Clai-w8w",
+  authDomain: "fireblogs-690f2.firebaseapp.com",
+  projectId: "fireblogs-690f2",
+  storageBucket: "fireblogs-690f2.appspot.com",
+  messagingSenderId: "759152541495",
+  appId: "1:759152541495:web:7a28b269c3c3a708dda4b2",
+};
+
+initializeApp(firebaseConfig);
+const timestamp = serverTimestamp;
+
+export { timestamp };
+export const dbService = getFirestore();
+
+/** 
+const db = getFirestore(firebaseApp);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, "cities");
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map((doc) => doc.data());
+  return cityList;
+}
+*/
